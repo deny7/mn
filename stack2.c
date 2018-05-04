6aad9072eb92317b68af64531fbbb47688e3774b
deny7 <around34@naver.com>
2018-05-05 00:01:46 +0900

    test

--- stack2.c
+++ stack2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int add (int num1, int num2){
     return num1, num2;
 }
@@ -14,6 +15,8 @@ int main(int argc, char*argv[]){
     num1 = 3;
     num2 = 5;
     result = add(num1, num2);
-    printf("%d\n, result");
-    return 0
+    printf("%d\n", result);
+    result = multiply(num1, num2);
+    printf("%d\n", result);
+    return 0;
 }
